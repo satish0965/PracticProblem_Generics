7e364141743b3b8b236f86248fa704ac9182f61e
Satish Patil <satish.patil7017@gmail.com>
2022-11-09 22:12:10 +0530

    Implimented Maximum Of three Float Numbers Problem solution

--- GenericsProblem/GenericsProblem/MaxUsingGenerics.cs
+++ GenericsProblem/GenericsProblem/MaxUsingGenerics.cs
@@ -9,7 +9,7 @@ namespace GenericsProblems
 
     public class FindMaxNum
     {
-        public  int GetMaximumInteger(int first_Number, int second_Number, int third_Number)
+        public int GetMaximumInteger(int first_Number, int second_Number, int third_Number)
         {
             if (first_Number.CompareTo(second_Number) > 0 && first_Number.CompareTo(third_Number) > 0)
                 return first_Number;
@@ -24,7 +24,27 @@ namespace GenericsProblems
             }
 
         }
+        //UC 2: Given 3 Floats Find The Maximum.
+        public double GetMaximumFloat(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("\teither 2 or 3 having same value/equals");
+                return 0.0;
+            }
 
+        }
     }
-
 }
--- GenericsProblem/GenericsProblem/Program.cs
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -1,31 +1,43 @@
-﻿
-namespace GenericsProblems
+﻿namespace GenericsProblems
 {
     class Program
     {
         public static void Main(string[] args)
         {
-          
-            Console.WriteLine("1. Find the Max Value Out Of Three:");
-           
+            Console.WriteLine("Please Choose The Option Can Be Executed: ");
+            Console.WriteLine("Find the Max Value Out Of Three: \n1.Integers \n2.Float \n3.Exit");
+            Console.Write("Enter the selected Option: ");
             int option = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the First Number: ");
-            int first_Number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Second Number: ");
-            int second_Number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Third Number: ");
-            int third_Number = Convert.ToInt32(Console.ReadLine());
+            FindMaxNum findMaxNum = new FindMaxNum();
 
             switch (option)
             {
                 case 1:
+                    Console.WriteLine("For Determining Max of Integer: ");
+                    Console.WriteLine("Enter the First Number: ");
+                    int first_Number = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Second Number: ");
+                    int second_Number = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Third Number: ");
+                    int third_Number = Convert.ToInt32(Console.ReadLine());
 
-                    FindMaxNum findMaxNum = new FindMaxNum();
                     if (findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number) != 0)
-                        Console.WriteLine("{0} is Maximum", findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number), first_Number, second_Number, third_Number);
+                        Console.WriteLine("{0} is Maximum out of {1} ,{2}, {3}", findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number), first_Number, second_Number, third_Number);
+
                     break;
                 case 2:
+                    Console.WriteLine("For Determining Max of Float: ");
+                    Console.WriteLine("Enter the First Number: ");
+                    double first_Num = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Second Number: ");
+                    double second_Num = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Third Number: ");
+                    double third_Num = double.Parse(Console.ReadLine());
+
+                    if (findMaxNum.GetMaximumFloat(first_Num, second_Num, third_Num) != 0)
+                        Console.WriteLine("{0} is Maximum out of {1} ,{2}, {3}", findMaxNum.GetMaximumFloat(first_Num, second_Num, third_Num), first_Num, second_Num, third_Num);
+                    break;
+                case 3:
                 default:
                     break;
             }
@@ -36,22 +48,24 @@ namespace GenericsProblems
 
 }
 
-//UC-1
+//UC-2
 //Given 3 Integers find the maximum
-//- Ensure to test code with the Test Case.
-//- To ensure your Code works you need 3 test cases with Max Number at 1st, 2nd and 3rd
-//- Use Integer Object and compareTo UC 1 method to test the maximum number
+//Given 3 Floats find the maximum
+//- Ensure to test code with the Test Case and repeat with similar 3 test cases in UC-1
 
 //Result
 
 //Please Choose The Option Can Be Executed:
-//1.Find the Max Value Out Of Three:
-//2.Exit:
-//Enter the selected Option: 1
+//Find the Max Value Out Of Three:
+//1.Integers
+//2.Float
+//3.Exit
+//Enter the selected Option: 2
+//For Determining Max of Float:
 //Enter the First Number:
-//23
+//4.8
 //Enter the Second Number:
-//32
+//5.9
 //Enter the Third Number:
-//8
-//32 is Maximum out of 23 ,32, 8
+//4.9
+//5.9 is Maximum out of 4.8 ,5.9, 4.9
