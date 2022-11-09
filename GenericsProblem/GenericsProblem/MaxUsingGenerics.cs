98b6ae0ee921dcba24be505bdfdb67f7f743e3e8
Satish Patil <satish.patil7017@gmail.com>
2022-11-09 21:59:49 +0530

    Determined Max of Interger

---
+++ GenericsProblem/GenericsProblem/MaxUsingGenerics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblems
+{
+
+    public class FindMaxNum
+    {
+        public  int GetMaximumInteger(int first_Number, int second_Number, int third_Number)
+        {
+            if (first_Number.CompareTo(second_Number) > 0 && first_Number.CompareTo(third_Number) > 0)
+                return first_Number;
+            else if (second_Number.CompareTo(first_Number) > 0 && second_Number.CompareTo(third_Number) > 0)
+                return second_Number;
+            else if (third_Number.CompareTo(first_Number) > 0 && third_Number.CompareTo(second_Number) > 0)
+                return third_Number;
+            else
+            {
+                Console.WriteLine("\tEither 2 or 3 having same value/equals");
+                return 0;
+            }
+
+        }
+
+    }
+
+}
---
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace GenericsProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+          
+            Console.WriteLine("1. Find the Max Value Out Of Three:");
+           
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the First Number: ");
+            int first_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            int second_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number: ");
+            int third_Number = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    FindMaxNum findMaxNum = new FindMaxNum();
+                    if (findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number) != 0)
+                        Console.WriteLine("{0} is Maximum", findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number), first_Number, second_Number, third_Number);
+                    break;
+                case 2:
+                default:
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+}
+
+//UC-1
+//Given 3 Integers find the maximum
+//- Ensure to test code with the Test Case.
+//- To ensure your Code works you need 3 test cases with Max Number at 1st, 2nd and 3rd
+//- Use Integer Object and compareTo UC 1 method to test the maximum number
+
+//Result
+
+//Please Choose The Option Can Be Executed:
+//1.Find the Max Value Out Of Three:
+//2.Exit:
+//Enter the selected Option: 1
+//Enter the First Number:
+//23
+//Enter the Second Number:
+//32
+//Enter the Third Number:
+//8
+//32 is Maximum out of 23 ,32, 8
